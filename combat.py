4dcf2e4ec20855574bbed079763532908110146e
WayfaringBloke <73042813+WayfaringBloke@users.noreply.github.com>
2023-11-22 20:10:23 +0000

    Update combat.py

--- combat.py
+++ combat.py
@@ -4,17 +4,21 @@ import random
 from time import sleep
 
 
-def droll(n):
-    r = 0 if n == 0 else random.randint(1, n)
-    print(f"Rolling d{n}", end="")
-    dots = random.randint(3, 5)
-    for _ in range(dots):
-        print(".", end="")
-        sleep(float(random.randint(3, 8)) / 50)
+def droll(n: int) -> int:
+    with open('output.txt', 'a') as f:
+        r = 0 if n == 0 else random.randint(1, n)
+        print(f"Rolling d{n}", end="")
+        print(f"Rolling d{n}", end="", file=f, flush=True)
+        dots = random.randint(3, 5)
+        for _ in range(dots):
+            print(".", end="")
+            print(".", end="", file=f, flush=True)
+            sleep(float(random.randint(3, 8)) / 50)
 
-    print(f"{' ' * (6 - dots)} got {r}!")
+        print(f"{' ' * (6 - dots)} got {r}!")
+        print(f"{' ' * (6 - dots)} got {r}!", file=f, flush=True)
     
-def roll(n):
+def roll(n: int) -> int:
     r = 0 if n == 0 else random.randint(1, n)
     print(f"Rolled d{n} got {r}")
     return r
@@ -26,28 +30,49 @@ def who_acts():
         print(f"Players act first")
 
 @dataclass
+class Armor:
+    resist: int
+    is_flat: bool
+
+    def blk(self):
+        return self.resist if self.is_flat else roll(self.resist)
+
+@dataclass
 class Weapon:
     damage: int
     is_auto: bool
+    is_flat: bool
 
     def atk(self, dr, name):
         for _ in range(3 if self.is_auto else 1):
-            dmg = roll(self.damage)
+            dmg = self.damage if self.is_flat else roll(self.damage)
             print(f"{name}: DR {dr} DMG {dmg}")
+
+class Name(str):
+    pass
+
+class HP(int):
+    pass
+
+class Morale(int):
+    pass
+
+class ToDodge(int):
+    pass
 
 @dataclass
 class Enemy:
-    name: str
-    hp: int
-    accuracy: int
-    morale: int
-    armor: int
+    name: Name
+    hp: HP
+    to_dodge: ToDodge
+    morale: Morale
+    armor: Armor
     weapon: Weapon
     group: object = field(init=False, repr=False)
-    strt_hp: int = field(init=False, repr=False)
+    start_hp: HP = field(init=False, repr=False)
 
     def dmg(self, amount):
-        blocked = roll(self.armor)
+        blocked = self.armor.blk()
         amount -= blocked
         if amount < 1:
             amount = 1
@@ -56,7 +81,7 @@ class Enemy:
         if self.hp <= 0:
             self.group.kill(self.name)
         elif self.hp <= self.strt_hp // 3:
-            print(f"{self.name}: BLOODIED")
+            print(f"{self.name}: BLOODIED (Rolling Morale)")
             self.mrl()
             self.strt_hp = 0
 
@@ -64,11 +89,10 @@ class Enemy:
         self.group.kill(self.name)
     
     def atk(self, mod):
-        dr = self.accuracy + mod
+        dr = self.to_dodge + mod
         self.weapon.atk(dr, self.name)
 
     def mrl(self):
-        print(f"{self.name}: MORALE")
         if roll(6) + roll(6) > self.morale:
             action = "FLEES" if roll(2) == 1 else "SURRENDERS"
             while True:
@@ -78,45 +102,25 @@ class Enemy:
             if inp.lower() != "n":
                 self.group.kill(self.name)
 
-
-    def ded(self):
-        return self.hp <= 0
-
     def __post_init__(self):
         self.strt_hp = self.hp
 
-@dataclass
 class Group:
-    leader: str
-    enemies: dict[str, Enemy]
-    initEnemies: int = field(init=False)
-    turnOrder: list[int] = field(init=False)
-    currentTurn: int = field(init=False)
+    leader: Name
+    enemies: dict[Name, Enemy]
+    initEnemies: int
 
-    def __post_init__(self):
-        self.initEnemies = len(self.enemies)
-        self.turnOrder = []
-        self.currentTurn = 0
+    def __init__(self, enemies: list[Enemy]):
+        self.leader = enemies[0].name \
+            if len(enemies) > 0 and enemies[0] != None else \
+            None
+        self.enemies = {}
+        self.initEnemies = 0
+        for enemy in enemies:
+            self.add(enemy)
 
     def get_enemy(self, name):
         return self.enemies[name]
-
-    def odr(self):
-        names = list(self.enemies.keys())
-        for i, name in enumerate(names):
-            print(f"[{i}]: {name}")
-        while True:
-            # parses comma separated indexes
-            trns = input("Turn order: ")
-            trns = list(map(int, trns.split(",")))
-            if sorted(trns) == list(range(len(names))):
-                break
-            print(f"Error: Invalid turn order {trns}")
-
-        self.turnOrder = [names[i] for i in trns]
-
-    def rst(self):
-        self.currentTurn = 0
 
     def run(self):
         if self.currentTurn >= len(self.turnOrder):
@@ -133,81 +137,100 @@ class Group:
         for e in self.enemies.values():
             e.mrl()
 
-    def kill(self, name):
-
+    def kill(self, name: Name):
         if name not in self.enemies:
-            print("EROR: Can't kill {name}")
+            print("Error: Can't kill {name}")
             return
 
-        killed = self.enemies[name]
         del self.enemies[name]
 
         print(f"{name}: DEAD")
 
         if self.leader == name:
+            print("LEADER DEAD: Rolling morale")
             self.mrl()
             self.leader = None
         if len(self.enemies) <= self.initEnemies // 2:
+            print("HALF DEAD: Rolling morale")
             self.mrl()
             self.initEnemies = 0
 
     def __getattr__(self, name):
         enemy = self.get_enemy(name)
         if enemy == None:
-            print(f"Enemy {name} not found")
+            print(f"Error: Enemy {name} not found")
         return enemy
+    
+    def add(self, enemy: Enemy, leader: bool = False):
+        if enemy.name in self.enemies:
+            print(f"Error: {enemy.name} already exists")
+        else:
+            self.enemies[enemy.name] = enemy
+            enemy.group = self
 
-PREFIXES = ["n:", "h:", "m:", "a:", "w:", "d:"]
+            if self.initEnemies > 0:
+                self.initEnemies += 1
 
-def remove_prefix(s, prf):
-    if not s.startswith(prf):
-        print(f"Error: {s} is missing prefix {prf}")
-    return s.removeprefix(prf)
+            if leader:
+                self.leader = enemy.name    
 
-def parse(s):
-    es = s.split("\n")
-    enemies = {}
-    leader = None
-    for i, e in enumerate(es):
-        eas = [remove_prefix(ea.strip(), prf).strip() for ea, prf in zip(e.split(","), PREFIXES)]
+def n(name: str) -> Name:
+    return Name(name)
 
-        name, hp, morale, rawArmor, rawWeapon, accuracy = eas
-        if i == 0:
-            leader = name
+def h(hp: int) -> HP:
+    return HP(hp)
 
-        hp = int(hp)
-        morale = int(morale)
-        armor = int(remove_prefix(rawArmor, "d").strip())
-        rawWeapon = remove_prefix(rawWeapon, "d").strip()
-        weaponDamage = int(rawWeapon.removesuffix("a"))
-        weaponAuto = rawWeapon.endswith("a")
-        weapon = Weapon(weaponDamage, weaponAuto)
-        accuracy = int(accuracy)
-        enemy = Enemy(
-            name,
-            hp,
-            accuracy,
-            morale,
-            armor,
-            weapon
-        )
+def m(morale: int) -> Morale:
+    return Morale(morale)
 
-        enemies[name] = enemy
-    group = Group(leader, enemies)
+def w(damage: int, is_auto: bool = False) -> Weapon:
+    return Weapon(damage, is_auto, False)
 
-    for e in enemies.values():
-        e.group = group
+def wf(damage: int, is_auto: bool = False) -> Weapon:
+    return Weapon(damage, is_auto, True)
 
-    pprint(group)
+def a(resist: int, is_flat: bool = True) -> Armor:
+    return Armor(resist, is_flat)
 
-    return group
+def d(to_dodge: int) -> ToDodge:
+    return ToDodge(to_dodge)
+
+def ne(*args):
+    STATS: dict[type, str]= {
+        Name: "name", HP: "hp", ToDodge: "to_dodge", 
+        Morale: "morale", Armor: "armor", Weapon: "weapon",
+    }
+    enemy_props = {}
+
+    for i, arg in enumerate(args):
+        if type(arg) in STATS:
+            prop = STATS[type(arg)]
+            if prop in enemy_props:
+                print(f"Error: {arg} [{i}] is a repeated {prop} value")
+                return None
+            enemy_props[prop] = arg
+        else:
+            print(f"Error: {arg} [{i}] is of type {type(arg)}")
+            return None
+        
+    for prop in STATS.values():
+        if prop not in enemy_props:
+            print(f"Error: {prop} not set")
+            return None
+    
+    return Enemy(**enemy_props)
+
+
+
 # format
 # groups of enemies are separated by line breaks
 # enemy stats are separated by commas
 # leader always first
 # e.g.
-"""n: fish, h: 10, m: 4, a: d0, w: d4a, d: 12
-n: gamer, h: 200, m: 11, a: d10, w: d6, d: 14"""
+Group([
+    ne(n("fish"), h(10), d(4), w(4, True), a(1), m(4)),
+    ne(n("gamer"), h(200), d(14), w(6), a(10, False),  m(11)),
+])
 
 # usage
 # 0. group = parse(string)
@@ -215,5 +238,3 @@ n: gamer, h: 200, m: 11, a: d10, w: d6, d: 14"""
 # 2. group.odr() to pick an attack order for the enemies
 # 3. group.name.dmg(amount) to damage enemies
 # 4. group.run() runs 1 enemies turn at a time
-
-
